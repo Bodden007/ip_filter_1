94df43784594a5dec375e5a16fdfcec6f7911bfc
Bodden007 <92564748+Bodden007@users.noreply.github.com>
2023-05-15 11:22:03 +0500

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/CPP_DEV/Learning/ip_filter
-# Build directory: C:/CPP_DEV/Learning/ip_filter/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
